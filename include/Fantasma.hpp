ff6e16f885f4bd28d01fe7991385d8dd94267f83
Your Name <you@example.com>
2024-06-26 20:27:29 -0600

    version 4

--- include/Fantasma.hpp
+++ include/Fantasma.hpp
@@ -1,20 +1,34 @@
+#include <SFML/Graphics.hpp>
+
 class Fantasma
 {
 private:
-    /* data */
+    sf::Texture imagen;
+    sf::Sprite sprite;
+    float velocidad;
 public:
-    Fantasma(/* args */) {}
+    Fantasma() {
+        imagen.loadFromFile("./images/fantasma.png");
+        velocidad = 0.1;
+        sprite.setTexture(imagen);
+        sprite.scale(sf::Vector2f(0.25f, 0.25f));
+    }
     ~Fantasma() {}
     void MoveRight(){
+        sprite.move(sf::Vector2f(velocidad, 0));
     }
     void MoveLeft(){
+        sprite.move(sf::Vector2f(velocidad*-1, 0));
     }
     void MoveUp(){
+        sprite.move(sf::Vector2f(0, 0));
     }
     void MoveDown(){
-
+        sprite.move(sf::Vector2f(0, velocidad));
+    }
+    void Draw(sf::RenderWindow &window){
+        window.draw(sprite);
     }
 
-    Draw 
 
 };
--- src/pruebaFantasma.cpp
+++ src/pruebaFantasma.cpp
@@ -1,13 +1,37 @@
+#include <SFML/Graphics.hpp>
 #include <Fantasma.hpp>
 
 int main(int argc, char const *argv[])
 {
     Fantasma fantasma;
-    fantasma.MoveDown();
 
-    
+    // Crear una ventana de SFML
+    sf::RenderWindow window(sf::VideoMode(800, 800), "SFML works!");
 
+    // Crear una forma circular de SFML
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // Verificar si se ha cerrado la ventana
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        fantasma.MoveDown();
+
+        // Limpiar la ventana
+        window.clear();
+
+        fantasma.Draw(window);
+
+        // Mostrar la ventana
+        window.display();
+    }
 
     return 0;
 }
